abb187147fba64b5c814ddef4254dbd44a1ea211
26ananya <das26ananya@gmail.com>
2019-02-19 19:01:09 +0530

    removed index.css and deleted code from app.css

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <h1>The App Component</h1>
+      </div>
+    );
   }
 }
 
